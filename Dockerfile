904d2d4f76072ae75457eb1d02ff1076a9461226
Cristian Paris <cristian.tanasep@gmail.com>
2023-01-11 13:23:00 +0200

    Changes

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
 FROM python:latest
 
 COPY requirements.txt /tmp
-RUN pip install -r /tmp/requirements.txt
+
+RUN pip3 install --upgrade pip
+RUN pip3 install -r /tmp/requirements.txt
+RUN apt-get update && apt-get install -y netcat
+
 
 WORKDIR /adapter
 COPY adapter.py /adapter
+COPY start.sh /adapter
 
-CMD ["python3", "-u", "adapter.py"]
+RUN chmod +x /adapter/start.sh
+
+CMD ["./start.sh"]
--- adapter.py
+++ adapter.py
@@ -1,8 +1,16 @@
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 from re import match
-import json
+from json import loads, dumps
 import logging
+from datetime import datetime
+from os import getenv
+
+INFLUX_HOST = "sprc3_influxdb"
+INFLUX_PORT = 8086
+BROKER_HOST = "sprc3_broker"
+BROKER_PORT = 1883
+DB_NAME = "influx_db"
 
 def on_connect(client, userdata, flags, rc):
     client.subscribe("#")
@@ -11,22 +19,65 @@ def on_message(client, userdata, msg):
     if not match(r'^[^/]+/[^/]+$', msg.topic):
         return
 
-    logging.info(f'Received a message by topic [{msg.topic}]')
-    process_message(msg)
+    if getenv('DEBUG_DATA_FLOW') == 'true':
+        logging.info(f'Received a message by topic [{msg.topic}]')
+    
+    process_message(msg, userdata)
 
-def process_message(msg):
+def process_message(msg, userdata):
     location, station = msg.topic.split('/')
-    payload = json.loads(msg.payload.decode('utf-8'))
+    payload = loads(msg.payload.decode('utf-8'))
+
+    db_data = []
+
+    try:
+        tstamp = datetime.strptime(payload['timestamp'], '%Y-%m-%dT%H:%M:%S%z')
+
+        if getenv('DEBUG_DATA_FLOW') == 'true':
+            logging.info(f'Data timestamp is : {tstamp}')
+        
+    except:
+        tstamp = datetime.now()
+        
+        if getenv('DEBUG_DATA_FLOW') == 'true':
+            logging.info('Data timestamp is NOW')
+
+    for key, val in payload.items():
+        if type(val) != int and type(val) != float:
+            continue
+
+        db_data.append({
+			'measurement': f'{station}.{key}',
+			'tags': {
+				'location': location,
+				'station': station
+			},
+			'time': tstamp.strftime('%Y-%m-%dT%H:%M:%S%z'),
+			'fields': {
+				'value': float(val)
+			}
+		})
+
+        if getenv('DEBUG_DATA_FLOW') == 'true':
+            logging.info(f'{location}.{station}.{key} {val}')
 
 
-
+    if (len(db_data) != 0):
+        userdata.write_points(db_data)
 
 def main():
-    client = mqtt.Client()
+    logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
+
+    db_client = InfluxDBClient(INFLUX_HOST, INFLUX_PORT)
+    db_client.create_database(DB_NAME)
+    
+    db_client.switch_database(DB_NAME)
+    db_client.create_retention_policy('unlimited', 'INF', 2, default=True)
+
+    client = mqtt.Client(userdata=db_client)
     client.on_connect = on_connect
     client.on_message = on_message
-
-    client.connect("broker", 1883, 60)
+    client.connect(BROKER_HOST, BROKER_PORT, 60)
     client.loop_forever()
 
 
